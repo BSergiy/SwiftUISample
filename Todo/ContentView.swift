a68a066eb117904f0bf6ff97149289a10699112e
Sergey Borodin <bellispiritus@gmail.com>
2019-10-25 16:42:53 +0300

    Initial Commit

---
+++ Todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by Сергей Бородин on 25.10.2019.
+//  Copyright © 2019 Сергей Бородин. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
